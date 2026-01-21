3e965bdd99ef7c585753e5ae03b719e86362dc2f
bhagyawan1817 <bhagyawansajjan@gmail.com>
2026-01-21 18:12:45 +0530

    che

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-// @SpringBootApplication
+@SpringBootApplication
 public class PetClinicApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PetClinicApplication.class, args);
+		SpringApplication.run(PetClinicApplication.class, args);;
+		123
 	}
 }
+
